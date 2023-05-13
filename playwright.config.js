f1433a78b65c9eed13d4ab480c57fe18070abaf9
Elias Papavasileiou <eliaspap@pm.me>
2023-05-13 11:37:46 +0300

    ci: use multiple workers, do not retry

--- playwright.config.js
+++ playwright.config.js
@@ -7,12 +7,10 @@ export default defineConfig({
   testDir: "./src/tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: "list",
   timeout: 60000,
   use: {
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
   },
   projects: [
     {
