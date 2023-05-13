21fde96ad5bc178e0d4651c8430bf0d801bf72fc
Elias Papavasileiou <eliaspap@pm.me>
2023-05-13 11:55:25 +0300

    Revert "ci: increase workers"
    This reverts commit 7c9a66c889c4e8e46e11491ed1255c1dd43a2f6f.

--- playwright.config.js
+++ playwright.config.js
@@ -6,7 +6,6 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./src/tests",
   fullyParallel: true,
-  workers: "100%",
   forbidOnly: !!process.env.CI,
   reporter: "list",
   timeout: 60000,
