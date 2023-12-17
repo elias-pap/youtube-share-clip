5d0767e4fbc73e139b575a47a66514e519449038
Elias Papavasileiou <eliaspap@pm.me>
2023-12-17 13:00:19 +0200

    fix(queries): make share button query search for share icon

--- src/constants/utils/queries.js
+++ src/constants/utils/queries.js
@@ -5,4 +5,4 @@ export const sleepTime = 200;
 export const pollingTimeoutInSeconds = 30;
 
 export const shareButtonSelector =
-  "#actions-inner #top-level-buttons-computed ytd-button-renderer button";
+  '#actions path[d="M15 5.63 20.66 12 15 18.37V14h-1c-3.96 0-7.14 1-9.75 3.09 1.84-4.07 5.11-6.4 9.89-7.1l.86-.13V5.63M14 3v6C6.22 10.13 3.11 15.33 2 21c2.78-3.97 6.44-6 12-6v6l8-9-8-9z"]';
