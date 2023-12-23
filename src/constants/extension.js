6af84964e3f4523fcc7b2104ca01b556f2d82585
Elias Papavasileiou <eliaspap@pm.me>
2023-12-23 02:08:09 +0200

    Add "End at" label translations (#48)
    * feat: add translation feature
    
    * feat: translate to 10 more languages

---
+++ src/constants/extension.js
@@ -0,0 +1,21 @@
+export const defaultEndAtLabelText = "End at";
+
+export const langToEndAtStringMap = new Map([
+  ["en", defaultEndAtLabelText],
+  ["en-GB", defaultEndAtLabelText],
+  ["en-IN", defaultEndAtLabelText],
+  ["el-GR", "Λήξη σε"],
+  ["es-ES", "Terminar en"],
+  ["es-419", "Terminar en"],
+  ["es-US", "Terminar en"],
+  ["fr-FR", "Fin à"],
+  ["fr-CA", "Fin à"],
+  ["zh-Hans-CN", "结束时间"],
+  ["zh-Hant-TW", "結尾處"],
+  ["hi-IN", "यहीं दौड़ समाप्त करें"],
+  ["de-DE", "Ende bei"],
+  ["it-IT", "Fine a"],
+  ["pt-PT", "Terminar em"],
+  ["pt-BR", "Terminar em"],
+  ["tr-TR", "Son:"],
+]);
