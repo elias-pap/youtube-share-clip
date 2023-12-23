d9ef8b998b6815a716a5bb2c1c2f2f669551c1b8
Elias Papavasileiou <eliaspap@pm.me>
2023-12-24 00:54:31 +0200

    feat(queries): use many getters in pollForElement()

--- src/constants/utils/queries.js
+++ src/constants/utils/queries.js
@@ -6,3 +6,5 @@ export const pollingTimeoutInSeconds = 30;
 
 export const shareButtonSelector =
   "#actions-inner #top-level-buttons-computed ytd-button-renderer button";
+export const shareButtonSelector2 =
+  "#actions-inner #top-level-buttons-computed yt-button-view-model button";
