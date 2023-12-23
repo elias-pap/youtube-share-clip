5b640376b07a777c3b2da5c13127a96f24290bdd
Elias Papavasileiou <eliaspap@pm.me>
2023-12-23 22:15:52 +0200

    test: enable trace on first retry

--- playwright.config.js
+++ playwright.config.js
@@ -7,12 +7,13 @@ export default defineConfig({
   testDir: "src/tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : undefined,
   workers: process.env.CI ? 1 : undefined,
   reporter: [["html", { open: "never" }]],
   timeout: 60000,
-  // use: {
-  //   trace: "retain-on-failure",
-  // },
+  use: {
+    trace: "on-first-retry",
+  },
   projects: [
     {
       name: "chromium",
